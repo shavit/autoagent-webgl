0650d48bdddedba8471ab54f79064119624b6c32
shavit <shavit@users.noreply.github.com>
2020-11-22 00:17:00 -0500

    distances

--- src/observation.js
+++ src/observation.js
@@ -2,6 +2,29 @@ import {
   Vector3,
 } from 'three';
 
-class Observation {
+export class Observation {
+  distanceTo(a, b) {
+    return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+  }
+
+  closestAgents(agent, items, n) {
+    const agents = [].concat(items);
+    agents.sort((a, b) => {
+      distanceTo(agent, a) - distanceTo(agent, b);
+    });
+
+    return agents.slice(1, n + 1);
+  }
+
+  move({agent, agents, delta}) {
+    for (const [index, peerAgent] of agents) {
+      const peerDistance = this.distanceTo(agent.getPosition(), peerAgent.getPosition())
+      if (peerDistance < 0.2) {
+        agent.steerAway()
+      }
+
+      agent.move({delta})
+    }
+  }
 
 }
